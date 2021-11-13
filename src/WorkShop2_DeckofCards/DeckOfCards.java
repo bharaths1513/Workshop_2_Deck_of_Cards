8ca88117ccb63a00c39716d8f9a287374f4e8687
bharaths1513 <bharaths.1311@gmail.com>
2021-11-13 20:18:18 +0530

    updated: UC2 no of players

--- src/WorkShop2_DeckofCards/DeckOfCards.java
+++ src/WorkShop2_DeckofCards/DeckOfCards.java
@@ -1,11 +1,16 @@
 package WorkShop2_DeckofCards;
 
+import java.util.Scanner;
+
 public class DeckOfCards {
 
-	String[] Deckofcards = new String[52];
+	Scanner scanner = new Scanner(System.in);
+	int pos = 0;
 
+// UC1 Initilizing Cards
 	public void initializingCards() {
-		int pos = 0;
+
+		String[] Deckofcards = new String[52];
 		String[] suits = { "Clubs", "Diamonds", "Hearts", "Spades" };
 		String[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
 
@@ -18,9 +23,22 @@ public class DeckOfCards {
 		}
 	}
 
+	public void noOfPlayers() {
+		System.out.print("\nEnter number of players minimum 2 , maximum 4 : ");
+		int player = scanner.nextInt();
+		if (player >= 2 && player <= 4) {
+			System.out.println("\n" + player + " players will play the game");
+		} else {
+			System.out.println("Please enter number of players in the Range");
+			this.noOfPlayers();
+
+		}
+	}
+
 	public static void main(String[] args) {
 		DeckOfCards o = new DeckOfCards();
 		o.initializingCards();
+		o.noOfPlayers();
 	}
 
 }
