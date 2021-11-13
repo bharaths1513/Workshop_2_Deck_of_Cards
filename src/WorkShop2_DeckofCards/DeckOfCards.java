e139f28fa8ec502d0b9422efd92066570703cc7e
bharaths1513 <bharaths.1311@gmail.com>
2021-11-13 20:41:17 +0530

    updated: Sequence of players

--- src/WorkShop2_DeckofCards/DeckOfCards.java
+++ src/WorkShop2_DeckofCards/DeckOfCards.java
@@ -1,28 +1,39 @@
 package WorkShop2_DeckofCards;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DeckOfCards {
 
 	Scanner scanner = new Scanner(System.in);
-	int pos = 0;
+	public ArrayList<String> cardsDeck = new ArrayList<String>();
 
 // UC1 Initilizing Cards
 	public void initializingCards() {
 
-		String[] Deckofcards = new String[52];
 		String[] suits = { "Clubs", "Diamonds", "Hearts", "Spades" };
-		String[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+		String[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "King", "Queen", "Ace" };
+		int numOfCards = suits.length * ranks.length;
+		System.out.println("\nNumber of cards in the deck is : " + numOfCards);
 
-		for (int i = 0; i < suits.length; i++) {
-			for (int j = 0; j < ranks.length; j++) {
-				Deckofcards[pos] = suits[i] + ranks[j];
-				pos++;
-				System.out.println(Deckofcards[pos] = suits[i] + ranks[j]);
+		for (int i = 0; i < ranks.length; i++) {
+
+			for (int j = 0; j < suits.length; j++) {
+				cardsDeck.add(ranks[i] + "----->" + suits[j]);
 			}
 		}
+		toDisplay(cardsDeck);
 	}
 
+	public static void toDisplay(ArrayList<String> cardsDeck) {
+		System.out.println("\nCards in Deck:");
+		for (String element : cardsDeck) {
+			System.out.println(element);
+		}
+		System.out.println();
+	}
+
+// UC2 no of players to play min players 2 and max players 4 
 	public void noOfPlayers() {
 		System.out.print("\nEnter number of players minimum 2 , maximum 4 : ");
 		int player = scanner.nextInt();
@@ -35,10 +46,20 @@ public class DeckOfCards {
 		}
 	}
 
+	// UC 3 sequence of the player.
+
+	public void sequenceOfPlay(int player) {
+		System.out.println("\nSequence of cards are below : ");
+		for (int i = 1; i <= player; i++) {
+			System.out.println("\nPlayer " + i + " Getting card.............");
+
+		}
+	}
+
 	public static void main(String[] args) {
 		DeckOfCards o = new DeckOfCards();
 		o.initializingCards();
 		o.noOfPlayers();
-	}
 
+	}
 }
